38099ee2ccde98dbfeed1319dff40d18546f63a3
2DimBer3 <berezan4@mail.ru>
2026-06-03 16:29:53 +0300

    fix "--no-sandbox"

--- src/test/java/hexlet/code/tests/BaseTest.java
+++ src/test/java/hexlet/code/tests/BaseTest.java
@@ -30,7 +30,7 @@ public abstract class BaseTest {
     @BeforeEach
     public void setupTest() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--nо-sandbox", "--headless");
+        options.addArguments("--no-sandbox", "--headless");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
